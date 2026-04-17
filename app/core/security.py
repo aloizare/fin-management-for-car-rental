ea35a6b6a2f94893cc9bd094c8362d4d0d03078d
Hellmina Enjelina Fitri <hellminae@gmail.com>
2026-04-17 21:09:31 +0700

    fix: move config to env, add session timeout

--- app/core/security.py
+++ app/core/security.py
@@ -11,7 +11,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 SECRET_KEY = os.getenv("SECRET_KEY")
 ALGORITHM = os.getenv("ALGORITHM")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
-SESSION_TIMEOUT_MINUTES = 30
+SESSION_TIMEOUT_MINUTES = int(os.getenv("SESSION_TIMEOUT_MINUTES"))
 
 
 def get_password_hash(password: str):
